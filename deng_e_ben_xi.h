5e6cfd49c6791fd41f2860732cc1a836c8f60638
Justin-G <starsguo@live.com>
2017-02-05 11:13:57 +0800

    等额本息类

---
+++ deng_e_ben_xi.h
@@ -0,0 +1,13 @@
+#ifndef __DENG_E_BEN_XI__
+#define __DENG_E_BEN_XI__
+#include "dai_kuan.h"
+
+class deng_e_ben_xi:public dai_kuan
+{
+	public:		
+    	virtual double calc_yuehuan();
+		virtual void show_all();
+};
+
+
+#endif
